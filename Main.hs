3e8e78c0f7735d65183b991446e5a91f07e1e42d
Vincent <vpark45@gmail.com>
2022-06-27 22:29:30 +0000

    Add Pair data type and its Arbitrary instance

--- Main.hs
+++ Main.hs
@@ -23,6 +23,20 @@ instance Arbitrary a => Arbitrary (Identity a) where
 identityGenInt :: Gen (Identity Int)
 identityGenInt = identityGen
 
+data Pair a b = Pair a b deriving (Eq, Show)
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Pair a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+  arbitrary = pairGen
+
+pairGenIntString :: Gen (Pair Int String)
+pairGenIntString = pairGen
+
 main :: IO ()
 main = do
   sample trivialGen
