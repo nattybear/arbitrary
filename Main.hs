8cfb15bd761281f4f2ecc640e572dd3415794a47
Vincent <vpark45@gmail.com>
2022-06-27 22:25:20 +0000

    Add Identity type and its Arbitrary instance

--- Main.hs
+++ Main.hs
@@ -10,6 +10,19 @@ trivialGen = return Trivial
 instance Arbitrary Trivial where
   arbitrary = trivialGen
 
+data Identity a = Identity a deriving (Eq, Show)
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+  a <- arbitrary
+  return (Identity a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = identityGen
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
 main :: IO ()
 main = do
   sample trivialGen
